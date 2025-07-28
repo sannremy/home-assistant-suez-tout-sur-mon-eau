92bb8599fa7666e65a030c234d66f8169613864d
sannremy <667617+sannremy@users.noreply.github.com>
2025-07-28 07:26:48 +0200

    Format data for Chart.js

--- suez.js
+++ suez.js
@@ -138,11 +138,15 @@ const getData = async () => {
               meter: liters,
               state_class: 'measurement',
               // Chart.js labels and datasets attributes
-              chart_labels: stats.map(stat => stat.date.substring(0, 10)), // YYYY-MM-DD
               chart_datasets: [{
                 type: 'line',
                 label: 'Water consumption',
-                data: stats.map(stat => stat.volume * 1000), // convert m3 to L
+                data: stats.map(stat => {
+                  return {
+                    x: stat.date,
+                    y: stat.volume * 1000, // convert m3 to L
+                  };
+                }),
               }],
             },
           }),
